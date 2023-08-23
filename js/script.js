42d58bfac357faa578339593df624669b06fd11d
Lcourse <101373417+Lcourse@users.noreply.github.com>
2023-08-22 23:35:22 -0300

    play / squares

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 let restartButton = document.createElement("button");
 let score = 0;
 let toWin = 0;
+
+document.getElementById("play").addEventListener("click", function game(){
+    const scoreDisplay = document.getElementById("score");
+    const width = 28; //28*28 = 784 squares
+    const grid = document.querySelector(".grid")
+    const layout = [
+        1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ]
+    const squares = [];
+})
